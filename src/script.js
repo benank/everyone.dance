7265b325760a3400ce605f714c8b4f0d7dfe6700
benank <terkolskie@gmail.com>
2021-01-24 22:07:18 -0800

    More updates

--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
 
     const IS_ELECTRON = typeof require != 'undefined';
 
+    $('input.gamecode').hide();
+    $('div.submit-gamecode-button').hide();
+
     // Electron-only code
     if (IS_ELECTRON)
     {
@@ -18,4 +21,47 @@ $(document).ready(function () {
         // Browser only code
         $('div.close-button').remove();
     }
+
+    function ToggleGameCodeDisplay(enabled)
+    {
+        if (enabled)
+        {
+            $('div.button.join').hide();
+            $('input.gamecode').show();
+            $('div.submit-gamecode-button').show();
+            $('input.gamecode').focus();
+        }
+        else
+        {
+            $('div.button.join').show();
+            $('input.gamecode').hide();
+            $('div.submit-gamecode-button').hide();
+            $('input.gamecode').blur();
+        }
+    }
+
+    // User clicks the join button
+    $('div.button.join').on('click', (e) => 
+    {
+        ToggleGameCodeDisplay(true);
+    })
+    
+    // User clicks the create button
+    $('div.button.create').on('click', (e) => 
+    {
+        ToggleGameCodeDisplay(false);
+    })
+    
+    // User clicks the install button
+    $('div.button.install').on('click', (e) => 
+    {
+        ToggleGameCodeDisplay(false);
+    })
+
+    $('input.gamecode').on('input keyup blur', function(e)
+    {
+        const input = $(this);
+        input.val(input.val().toUpperCase().replace(/[^A-Z]/g,'') );
+    })
+
 })
