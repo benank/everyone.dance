f45b931f9f4d70ba26c46f359d637afaf62a77f9
benank <terkolskie@gmail.com>
2021-07-29 09:45:39 -0700

    Update version

--- src/js/constants/version.js
+++ src/js/constants/version.js
@@ -1,3 +1,3 @@
 import {isWebVersion} from "./isWebVersion";
-const VERSION_BASE = "2.3.15"
+const VERSION_BASE = "2.3.16"
 export const VERSION = (!isWebVersion && electron.isDev) ? VERSION_BASE + "-dev" : VERSION_BASE;
