fba116aa8e4bf8915e34f47ade0b0072aae8f361
benank <terkolskie@gmail.com>
2021-07-30 07:51:49 -0700

    Undo dev version

--- src/js/constants/version.js
+++ src/js/constants/version.js
@@ -1,3 +1,3 @@
 import {isWebVersion} from "./isWebVersion";
-const VERSION_BASE = "2.3.17-dev"
+const VERSION_BASE = "2.3.17"
 export const VERSION = (!isWebVersion && electron.isDev) ? VERSION_BASE + "-dev" : VERSION_BASE;
